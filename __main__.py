eb5a26ccbaf2cb5e65a632013e2b0c90b3122aab
qwark97 <marcin.plata1.konta@gmail.com>
2021-05-22 22:57:03 +0200

    introduce first version of assumptions (might change during working on project)

--- __main__.py
+++ __main__.py
@@ -5,13 +5,37 @@ from business.business_state import Model
 
 def run():
     first_stage_assumptions = StageAssumptions(
-        # TODO
+        lost_income_percentage=(5, 2),
+        natural_loss_percentage=(7, 3),
+        clients_number=(15, 2),
+        average_income_per_client_number=(4000, 1000),
+        average_margin_per_client_percentage=(20, 5),
+        yearly_tax_percentage=20,
+        unexpected_cost_probability_percentage=(3, 10),
+        unexpected_costs_number=(1000, 3000),
+        fixed_costs_number=(15000, 1000),
     )
     second_stage_assumptions = StageAssumptions(
-        # TODO
+        lost_income_percentage=(6, 2),
+        natural_loss_percentage=(6, 2),
+        clients_number=(20, 3),
+        average_income_per_client_number=(4300, 700),
+        average_margin_per_client_percentage=(20, 6),
+        yearly_tax_percentage=29,
+        unexpected_cost_probability_percentage=(3, 5),
+        unexpected_costs_number=(1500, 2000),
+        fixed_costs_number=(17000, 1000),
     )
     third_stage_assumptions = StageAssumptions(
-        # TODO
+        lost_income_percentage=(7, 1),
+        natural_loss_percentage=(5, 2),
+        clients_number=(25, 4),
+        average_income_per_client_number=(5200, 500),
+        average_margin_per_client_percentage=(20, 4),
+        yearly_tax_percentage=35,
+        unexpected_cost_probability_percentage=(1, 1),
+        unexpected_costs_number=(10000, 1000),
+        fixed_costs_number=(20000, 1000),
     )
     initial_assets = Assets(
         # TODO
