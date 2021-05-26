97555e9aac3126ff04b935188f73e1c515d5b8df
qwark97 <marcin.plata1.konta@gmail.com>
2021-05-26 22:49:01 +0200

    for simplicity, replace balance only with assets

--- business_state.py
+++ business_state.py
@@ -3,11 +3,11 @@ from business.balance import Assets
 
 
 class Model:
-    def __init__(self, balance: Assets, assumptions: StageAssumptions):
+    def __init__(self, assets: Assets, assumptions: StageAssumptions):
         self.__stage = 1
         self.__year = 0
         self.__assumptions = assumptions
-        self.__balance = balance
+        self.__assets = assets
 
     def next_stage(self, assumptions: StageAssumptions):
         self.__stage += 1
